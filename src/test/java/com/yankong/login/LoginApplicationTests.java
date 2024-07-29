58111ba888d381f62e18e2172d526c6db8196192
pokly0908 <pokly0908@naver.com>
2024-07-29 10:58:58 +0900

    :sparkles: init project

---
+++ src/test/java/com/yankong/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yankong.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
